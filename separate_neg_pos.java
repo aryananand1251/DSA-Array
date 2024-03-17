68f11277315c3e67e1fd780b3da66d8c77069108
Aryan Anand <83996457+aryananand1251@users.noreply.github.com>
2024-03-17 17:55:30 +0530

    Separate negative and positive number in O(n) T.C

---
+++ separate_neg_pos.java
@@ -0,0 +1,36 @@
+package Start_java;
+
+import java.util.Scanner;
+
+public class separate_neg_pos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[10];
+        int max=Integer.MIN_VALUE;
+
+        System.out.println("Enter the elements in array");
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]<0)
+            {
+                arr[i]=arr[i];
+            }
+            else {
+               int temp=arr[i];
+               arr[i]=arr[n-1];
+               arr[n-1]=temp;
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
